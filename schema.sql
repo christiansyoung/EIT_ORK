24887cbd607d024d6cb4bc3f92dd50f36b64df68
Christian Strand Young <christian@strandyoung.com>
2014-03-07 11:54:50 +0100

    Fixed error in schema

--- schema.sql
+++ schema.sql
@@ -22,14 +22,14 @@ name TEXT NOT NULL
 );
 
 CREATE TABLE state (
-window_id INT PRIMARY KEY NOT NULL,
+window_id INTEGER PRIMARY KEY NOT NULL,
 open BOOLEAN,
 auto BOOLEAN,
 timer_id INT
 );
 
 CREATE TABLE timer (
-id INT PRIMARY KEY AUTOINCREMENT,
+id INTEGER PRIMARY KEY AUTOINCREMENT,
 window_id INT NOT NULL,
 hour INT NOT NULL,
 minute INT NOT NULL
@@ -37,3 +37,4 @@ minute INT NOT NULL
 
 INSERT INTO window (name) VALUES ('Window 1');
 INSERT INTO configuration (window_id, name, width, height, angle, enginepower, draftthreshold) VALUES (1,'',0,0,0,0,0);
+INSERT INTO state (window_id, open, auto, timer_id) VALUES (1,0,1,0);
