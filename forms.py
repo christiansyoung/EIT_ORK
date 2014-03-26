c7e88e100d99bdc4620b3ad9ec4d7f8288ab7b41
Christian Strand Young <christian@strandyoung.com>
2014-03-26 10:40:14 +0100

    Fixed regex

--- forms.py
+++ forms.py
@@ -12,5 +12,5 @@ class ConfigForm(Form):
     window_height = TextField('window_height', validators = [validators.Regexp('\d+((\.)\d+)?$')])
     area = TextField('area', validators = [validators.Regexp('^\d+$')])
     window_direction = SelectField('window_direction', coerce = int, choices = [(0, 'North'), (45, 'North East'), (90, 'East'), (135, 'South East'), (180, 'South'), (225, 'South West'), (270, 'North West')])
-    draft = TextField('draft', validators = [validators.Regexp('^\d+((,|\.)\d+)?$')])
+    draft = TextField('draft', validators = [validators.Regexp('^\d+((\.)\d+)?$')])
     window_hinge = SelectField('window_hinge', coerce=int, choices = [(1, 'Left'), (2, 'Right')])
