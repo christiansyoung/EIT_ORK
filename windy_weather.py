1f9feadd6e819a8990b98c53203a041e558e56b7
Magnus S. Thrap <magnus.solheim@gmail.com>
2014-03-26 14:40:11 +0100

    windy_weather.py sends data every 5th sec

--- windy_weather.py
+++ windy_weather.py
@@ -1,6 +1,12 @@
 import os
 from config import SERVICE_URL
+import threading
 
-os.system('curl -X POST -H "Content-Type: application/json" -d \''
-          '{"pressure": 1000, "wind": {"angle": 300, "speed": 29}, "temp": 15, "humidity": 79}\' '
-          'http://%s/api/weather_sensor_data' % SERVICE_URL)
+
+def send_data():
+    os.system('curl -X POST -H "Content-Type: application/json" -d \''
+              '{"pressure": 1000, "wind": {"angle": 300, "speed": 29}, "temp": 15, "humidity": 79}\' '
+              'http://%s/api/weather_sensor_data' % SERVICE_URL)
+    threading.Timer(5, send_data).start()
+
+send_data()
